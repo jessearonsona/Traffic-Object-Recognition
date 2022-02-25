53d8a2478ed50e358a5c9b6d9500976c3aed77b9
Nattybyw <boonyawee.natty@gmail.com>
2022-02-24 23:14:48 -0600

    add icon
    add icon on the left top of login page

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,15 +1,24 @@
 import "./Header.css";
 import logo from "./headerLogo.png";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {IconButton} from '@material-ui/core'
+
 
 const Header = () => {
   return (
     <div id="siteHeader">
-        <div id="headerTop">
+        <div id="headerTop">        
             <img src={logo} id="headerLogo" alt="NDSU" />
+             
+            <IconButton id="accountIcon"> 
+            <AccountCircleIcon fontSize= "large"/>
+            </IconButton> 
+
         </div>
         <div id="headerBottom">
             Upper Great Plains Transportation Institute
         </div>
+        
     </div>
   );
 };
