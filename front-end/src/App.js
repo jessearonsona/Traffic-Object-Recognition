567cb8dcba5666253fc447c55bc576b21f8105a5
Nattybyw <boonyawee.natty@gmail.com>
2022-04-06 21:22:44 -0500

    Dropdown and request password
    Dropdown and request password pop up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,8 @@ import { createTheme, ThemeProvider } from "@material-ui/core";
 import Login from "./pages/Login";
 import Tracking from "./pages/Tracking";
 import Conditions from "./pages/Conditions";
+import ResetPW from "./pages/ResetPW";
+import Account from "./pages/Account";
 
 const theme = createTheme({
   palette: {
@@ -23,6 +25,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/tracking" element={<Tracking />} />
           <Route path="/conditions" element={<Conditions />} />
+          <Route path="/ResetPW" element={<ResetPW />} />
+          <Route path="/Account" element={<Account />} />
         </Routes>
       </Router>
     </ThemeProvider>
